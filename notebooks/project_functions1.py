ae722383c05623dc0d5ac370b479fdc7cbb64c9a
Daniel <dkrasnov@student.ubc.ca>
2021-10-28 13:38:30 -0800

    Began answering research question in Task1, plotted all 'good' and 'best' restaurants using folium

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -1,5 +1,3 @@
-#Import Libraries
-import numpy as np
 import pandas as pd
 
 def clean_and_merge_zomato(zomato_file_path, country_codes_file_path):
@@ -14,23 +12,28 @@ def clean_and_merge_zomato(zomato_file_path, country_codes_file_path):
     
     #Method chain 1 - load in/clean zomato.csv
     zomato_df = (
-     pd.DataFrame(pd.read_csv(zomato_file_path, encoding = "maclatin2"))
+     pd.DataFrame(pd.read_csv("..\\data\\raw\\zomato.csv" , encoding = "maclatin2"))
      .drop(["Address","Rating color","Locality","Locality Verbose","Switch to order menu","Restaurant ID"],axis = 'columns')
      .dropna()
+     .rename(columns={"Average Cost for two": "Average Cost for Two", 
+                      "Has Table booking": "Has Table Booking",
+                      "Has Online delivery": "Has Online Delivery",
+                      "Rating text": "Rating Text",
+                      "Is delivering now": "Is Delivering Now",
+                      "Price Range": "Price Range",
+                      "Aggregate rating": "Aggregate Rating",})
     )
 
     #Method chain 2 - load in Country-Code.xlsx
     country_df = (
-        pd.DataFrame(pd.read_excel(country_codes_file_path))
+        pd.DataFrame(pd.read_excel("..\\data\\raw\\Country-Code.xlsx"))
     )
 
     #Method chain 3 - process/wrangle/merge country_df and zomato_df
     zomato_cleaned = (
      pd.merge(zomato_df,country_df, how = "inner",on = "Country Code")
-     #zomato_df = zomato_df[(zomato_df['Aggregate rating'].isin([0.0])) == False]
-     .set_index(['Country','City']) 
      .drop(['Country Code'], axis = 'columns')
-    .query('Country=="India"')
+     .query('Country=="India"')
     )
     
     return zomato_cleaned
