6eccdb7ee71c0a56e09e64dfeeaf98800455e14d
Daniel <dkrasnov@student.ubc.ca>
2021-10-27 18:46:09 -0800

    Finished Tak 3

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -14,21 +14,23 @@ def clean_and_merge_zomato(zomato_file_path, country_codes_file_path):
     
     #Method chain 1 - load in/clean zomato.csv
     zomato_df = (
-     pd.DataFrame(pd.read_csv(zomato_file_path , encoding = "maclatin2")) #TODO fix encoding
-     .drop(["Address","Rating color","Rating text","Locality","Locality Verbose","Switch to order menu","Average Cost for two"],axis = 'columns')
+     pd.DataFrame(pd.read_csv(zomato_file_path, encoding = "maclatin2"))
+     .drop(["Address","Rating color","Locality","Locality Verbose","Switch to order menu","Restaurant ID"],axis = 'columns')
      .dropna()
     )
-    
-    #Method chain 2 - load in Country-Code.xlsx 
+
+    #Method chain 2 - load in Country-Code.xlsx
     country_df = (
         pd.DataFrame(pd.read_excel(country_codes_file_path))
     )
-    
-    #Method chain 3 process/wrangle/merge country_df and zomato_df
+
+    #Method chain 3 - process/wrangle/merge country_df and zomato_df
     zomato_cleaned = (
      pd.merge(zomato_df,country_df, how = "inner",on = "Country Code")
+     #zomato_df = zomato_df[(zomato_df['Aggregate rating'].isin([0.0])) == False]
      .set_index(['Country','City']) 
      .drop(['Country Code'], axis = 'columns')
+    .query('Country=="India"')
     )
     
     return zomato_cleaned
